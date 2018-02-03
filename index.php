b6117aa838689699dc4dc943f6e4489e6f8db286
Rodrigo Portas <hoready_magrao@hotmail.com>
2018-02-03 14:25:21 -0200

    resolvi o conflito de olas

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Ola Mundo 456!";
 
-	echo "ola UDEMY";
-=======
-	echo "Ola Mundo 123!";
+	echo "ola UDEMY e ola HCODE";
 
-	echo "ola HCODE";
->>>>>>> 642fd477c0d605bd279173295ee4e5848a47457f
 
 
 ?>
